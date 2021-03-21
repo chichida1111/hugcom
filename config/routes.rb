ca342cc0565399ba016d590e5a9f68a6a8835d49
chichida1111 <choose.life.for.g@gmail.com>
2021-03-21 13:19:28 +0900

    ユーザー登録機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+ root to: "contacts#index"
+
+  devise_for :parent_users, controllers: {
+    sessions: 'parent_users/sessions',
+    registrations: 'parent_users/registrations'
+  }
+
+  devise_for :teacher_users, controllers: {
+    sessions: 'teacher_users/sessions',
+    registrations: 'teacher_users/registrations'
+  }
+
+  resources :contacts
+
 end
