20ec463e2794c8e054374d0b1742a80e16b2b00d
chichida1111 <choose.life.for.g@gmail.com>
2021-03-22 12:25:06 +0900

    テストコード実施

---
+++ spec/factories/contacts.rb
@@ -0,0 +1,15 @@
+FactoryBot.define do
+  factory :contact do
+    last_name_c_j         { '名字' }
+    first_name_c_j        { '名前' }
+    last_name_c_k         { 'ナマエ' }
+    first_name_c_k        { 'ミョウジ' }
+    group_id              { 2 }
+    association :parent_user
+
+    after(:build) do |contact|
+      contact.image.attach(io: File.open('app/assets/images/KOSODATE_logo.png'), filename: 'KOSODATE_logo.png')
+    end
+
+  end
+end
